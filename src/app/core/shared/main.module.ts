2472bb0814631e456ea5187206a36c5fe80ba9b6
unknown <abanoubsamir3004@gmail.com>
2022-12-29 04:39:51 +0200

    ds

--- src/app/core/shared/main.module.ts
+++ src/app/core/shared/main.module.ts
@@ -1,71 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   FaIconLibrary,
-  FontAwesomeModule,
+  FontAwesomeModule
 } from '@fortawesome/angular-fontawesome';
 import {
-  faAngleDown,
-  faAngleLeft,
-  faAngleRight,
   faAnglesLeft,
-  faAnglesRight,
-  faArrowLeft,
-  faArrowLeftLong,
-  faArrowRightLong,
-  faBars,
-  faBarsStaggered,
-  faBugs,
-  faChartColumn,
-  faCheck,
-  faCircleCheck,
-  faCircleMinus,
-  faCirclePlus,
-  faCircleXmark,
-  faCow,
-  faEarthAsia,
-  faEye,
-  faFileExcel,
-  faCalendar,
-  faFilePdf,
-  faFileWord,
-  faGear,
-  faGears,
-  faHouse,
-  faHouseChimneyUser,
-  faListCheck,
-  faLungsVirus,
-  faMap,
-  faMapLocationDot,
-  faMortarPestle,
-  faNoteSticky,
-  faPaperPlane,
-  faPencil,
-  faPenToSquare,
-  faPlus,
-  faReply,
-  faScrewdriverWrench,
-  faSyringe,
-  faTrash,
-  faTrashCan,
-  faTriangleExclamation,
-  faUserGear,
-  faViruses,
-  faWandMagicSparkles,
-  faWrench,
-  faXmark,
-  faVirus,
-  faPowerOff,
-  faUser,
-  faUnlock,
-  faUnlockKeyhole,
-
+  faAnglesRight, faHome, faListCheck
 } from '@fortawesome/free-solid-svg-icons';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { PrimeNGModule } from './primeNG.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { CommonModule } from '@angular/common';
+import { PrimeNGModule } from './primeNG.module';
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
@@ -98,62 +45,11 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 export class MainModule {
   constructor(library: FaIconLibrary) {
     library.addIcons(
-      faPlus,
-      faEye,
-      faPencil,
-      faTrash,
-      faScrewdriverWrench,
-      faWrench,
-      faCow,
-      faMortarPestle,
-      faBugs,
-      faEarthAsia,
-      faViruses,
-      faLungsVirus,
-      faSyringe,
-      faHouse,
-      faChartColumn,
-      faMap,
-      faCircleMinus,
-      faCircleXmark,
-      faCircleCheck,
-      faCirclePlus,
-      faFileWord,
-      faFilePdf,
-      faFileExcel,
-      faTriangleExclamation,
-      faAngleRight,
-      faAngleLeft,
-      faWandMagicSparkles,
-      faMapLocationDot,
-      faHouseChimneyUser,
-      faReply,
-      faCheck,
-      faCalendar,
-      faXmark,
-      faPaperPlane,
-      faBars,
-      faPenToSquare,
-      faTrashCan,
-      faAngleDown,
-      faBarsStaggered,
-      faArrowLeftLong,
-      faArrowRightLong,
-      faUserGear,
-      faGears,
-      faNoteSticky,
-      faGears,
-      faGear,
+      faHome,
       faListCheck,
       faAnglesLeft,
       faAnglesRight,
-      faArrowLeft,
-      faVirus,
-      faPencil,
-      faPowerOff,
-      faUser,
-      faUnlockKeyhole
-
     );
   }
 }
+
--- src/app/core/shared/primeNG.module.ts
+++ src/app/core/shared/primeNG.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ChartModule } from 'primeng/chart';
-import { DialogModule } from 'primeng/dialog';
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 import { PasswordModule } from 'primeng/password';
-import { DropdownModule } from 'primeng/dropdown';
-import { CalendarModule } from 'primeng/calendar';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { FileUploadModule } from 'primeng/fileupload';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { PaginatorModule } from 'primeng/paginator';
-import { GalleriaModule } from 'primeng/galleria';
-import { CheckboxModule } from 'primeng/checkbox'
-import { ToggleButtonModule } from 'primeng/togglebutton';
-import { CardModule } from 'primeng/card';
-import {RatingModule} from 'primeng/rating';
+import { RatingModule } from 'primeng/rating';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   imports: [],
@@ -31,15 +21,6 @@ import {RatingModule} from 'primeng/rating';
     ToastModule,
     MessageModule,
     PasswordModule,
-    DropdownModule,
-    CalendarModule,
-    ProgressSpinnerModule,
-    FileUploadModule,
-    MultiSelectModule,
-    PaginatorModule,
-    GalleriaModule,
-    CheckboxModule,
-    ToggleButtonModule
   ],
   declarations: [],
 })
