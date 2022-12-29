117d675cedb478297a0a97f2eef445bf400ae05b
unknown <abanoubsamir3004@gmail.com>
2022-12-29 06:30:33 +0200

    hjv

--- src/app/views/pages/auth/register/register.component.ts
+++ src/app/views/pages/auth/register/register.component.ts
@@ -1,17 +1,13 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
-  FormBuilder,
   FormControl,
   FormGroup,
-  Validators,
+  Validators
 } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
 import { MainService } from '@modules/main.service';
 import { ErrorInterface } from 'app/core/interceptor/error.interface';
 import { Login } from 'app/core/models/auth';
 import { AuthService } from 'app/core/services/auth.service';
-import { MessageService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-register',
@@ -47,15 +43,10 @@ export class RegisterComponent implements OnInit {
     },
   };
 
-  loginData: {
-    username: string;
-    password: string;
-  } = { username: '', password: '' };
+
   constructor(
     private authService: AuthService,
     public mainService: MainService,
-    private route: ActivatedRoute,
-    private router: Router
   ) {}
 
   registerForm = new FormGroup({
@@ -67,41 +58,8 @@ export class RegisterComponent implements OnInit {
     phone: new FormControl('', [Validators.required]),
   });
 
-  get login() {
-    return this.registerForm.controls;
-  }
-
   ngOnInit() {}
-  Login() {
-    this.submit = true;
-    if (this.registerForm.controls.username.value) {
-      this.loginData.username = this.registerForm.controls.username.value;
-    }
-    if (this.registerForm.controls.password.value) {
-      this.loginData.password = this.registerForm.controls.password.value;
-    }
-    this.authService.register(this.loginData).subscribe(
-      (res: Login) => {
-        localStorage.setItem('token', `Bearer ${res.token}`);
-      },
-      (error: ErrorInterface) => {
-        this.mainService.handleError(error);
-        error.errors.forEach((err: any) => {
-          this.mainService.showErrorMSG(err);
-        });
-      },
-      () => {
-        this.submit = false;
-        let pageBeforLogin = this.route.snapshot.queryParams['returnUrl'];
-        if (pageBeforLogin) this.router.navigateByUrl(pageBeforLogin);
-        else {
-          this.router.navigateByUrl('/');
-        }
-        this.mainService.showSuccessMSG('Logined Successfuly');
-      }
-    );
-  }
-
+ 
   Register() {
     this.submit = true;
     this.registerForm.controls.name.patchValue(this.name);
@@ -120,7 +78,6 @@ export class RegisterComponent implements OnInit {
         },
         () => {
           this.mainService.showSuccessMSG('Register Successfuly');
-          this.Login();
         }
       );
     } else return;
